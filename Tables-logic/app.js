70a4ad3364b37efaf4ad27a7a957e9b91eb5ddca
Muhammad Murtaza <121543125+Webeyes457@users.noreply.github.com>
2023-03-09 09:00:56 +0500

    INITIAL

---
+++ Tables-logic/app.js
@@ -0,0 +1,8 @@
+
+//                                Tables
+
+
+var num=prompt("Enter a num")
+
+            document.write(num+ "  x " + " 1 " + "= "+ num*1+ "<br>" + num+ " x " + " 2" + "= "+ num*2+ "<br>"+num+ "  x " + " 3 " + "= "+ num*1+ "<br>" +num+ " x " + " 4 " + "= "+ num*1+ "<br>"+num+ "  x " + " 5 " + "= "+ num*1+ "<br>"+num+ " x " + " 6 " + "= "+ num*1+ "<br>"+num+ "  x " + " 7 " + "= "+ num*1+ "<br>"+num+ " x " + " 8 " + "= "+ num*1+ "<br>"+num+ "  x " + " 9 " + "= "+ num*1+ "<br>"+num+ " x " + " 10 " + "= "+ num*1
+            )
